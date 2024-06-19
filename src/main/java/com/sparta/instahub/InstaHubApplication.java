bec081f2c0609f008132549878abd38a90c48c79
dami0806 <dami0804@naver.com>
2024-06-19 14:44:48 +0900

    git init

---
+++ src/main/java/com/sparta/instahub/InstaHubApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.instahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/instahub/InstaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.instahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
