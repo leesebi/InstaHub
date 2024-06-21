4e73697051ffdfeeb8ca7a0d968ce4fd21f9932a
dami0806 <dami0804@naver.com>
2024-06-21 10:04:18 +0900

    feat: s3 setting

--- src/main/java/com/sparta/instahub/post/dto/PostRequestDto.java
+++ src/main/java/com/sparta/instahub/post/dto/PostRequestDto.java
@@ -1,16 +1,17 @@
 package com.sparta.instahub.post.dto;
 
 import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 public class PostRequestDto {
     private String title; // 게시물 제목
     private String content; // 게시물 내용
-    private String imageUrl;
+    private MultipartFile imageUrl;
 
 
     // 필드를 초기화
-    public PostRequestDto(String title, String content, String imageUrl) {
+    public PostRequestDto(String title, String content, MultipartFile imageUrl) {
         this.title = title;
         this.content = content;
         this.imageUrl = imageUrl;
