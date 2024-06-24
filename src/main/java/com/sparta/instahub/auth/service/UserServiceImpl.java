25a3029c8db4a194fbde8e20b3607d93347dce5e
dami0806 <dami0804@naver.com>
2024-06-24 22:48:38 +0900

    fix: find repository by name

--- src/main/java/com/sparta/instahub/auth/service/UserServiceImpl.java
+++ src/main/java/com/sparta/instahub/auth/service/UserServiceImpl.java
@@ -70,7 +70,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public LoginResponse login(LoginRequest loginRequest) {
-        User user = userRepository.findByUserId(loginRequest.getUsername())
+        User user = userRepository.findByUsername(loginRequest.getUsername())
                 .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
 
         if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
